ce3cdf65f59b0868765b75e38ff3c2d3bebbd6ba
Gaurav Jha <80048862+GAURAV5925@users.noreply.github.com>
2023-02-14 20:18:30 +0530

    Update swapping.py

--- Day1/swapping.py
+++ Day1/swapping.py
@@ -9,9 +9,19 @@ b=temp
 print(a,b)
 
 
-# without temp
+# without using temp variable
 
 a= a+b
 b=a-b
 a=a-b
+print(a,b)
+
+#Third wasy of swapping
+
+a = 10
+b = 30
+
+print(a,b)
+[a,b] = [b,a] #using list/tupple we can also swap
+
 print(a,b)
