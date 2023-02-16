c3974191f73ebfed3f04f3dfbd86d6d1d74331fe
Gaurav Jha <80048862+GAURAV5925@users.noreply.github.com>
2023-02-16 11:39:10 +0530

    Create Day4session1.py

---
+++ Day4session1.py
@@ -0,0 +1,43 @@
+'''def name():
+    print("my name is gaurav")
+    
+name()'''
+
+
+'''
+def login():
+    
+    while True:
+        username = input("Enter username")
+        password = input("Enter the password")
+        if username==password:
+            print("login successfully")
+            break
+        else:
+            print("login failed please enter correct details")
+            
+login()
+'''
+
+
+'''
+import time
+
+def welcome():
+    print("Hello Python")
+    
+print("First time")
+time. sleep(2)
+welcome ()
+print("Second time")
+time. sleep(3)
+welcome ()
+'''
+
+def info(firstname, lastname):
+    print("First Name",firstname)
+    print("Last Name", lastname)
+    
+info("gaurav","jha")
+info(firstname="jha",lastname="gaurav")
+            
