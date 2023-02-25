e8c2d4621d9339e8ef75f4217206d9c4692239ec
Gaurav Jha <80048862+GAURAV5925@users.noreply.github.com>
2023-02-26 00:12:45 +0530

    Update FindingPercentage.py

--- HackerRank/Day1/FindingPercentage.py
+++ HackerRank/Day1/FindingPercentage.py
@@ -2,10 +2,12 @@ if __name__ == '__main__':
     n = int(input())
     student_marks = {}
     for m in range(n):
-        name, *line = input().split()
+        name, *line = input().split()    # *line will make the imput in same line but after the space it will taken in the list as a string
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
     marks = student_marks[query_name]
     
-    print(format(sum(marks)/3,'.2f'))
+    print(format(sum(marks)/3,'.2f'))  #SYNTAX --> fromat(value,'.nf')   .nf is in the string format
+    
+    #This formate will make the float value upto 2 digit
