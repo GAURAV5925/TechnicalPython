135463b8a133895fe12371b7452538e285b56cf8
Gaurav Jha <80048862+GAURAV5925@users.noreply.github.com>
2023-02-26 00:06:44 +0530

    Update Nested.py

--- HackerRank/Day1/Nested.py
+++ HackerRank/Day1/Nested.py
@@ -4,19 +4,19 @@ if __name__ == '__main__':
     for n in range(int(input())):
         name = input()
         score = float(input())
-        res.append([name,score])
-        grade.append(score)
+        res.append([name,score])   #Here we are creating a list inside a list in which we are storing the value of the name and score
+        grade.append(score)      #We are creating a another list in which we are storing the scores
         
-grades = sorted(set(grade))
-m = grades[1]
+grades = sorted(set(grade))  #Sorted can be used to sort all LIST / DICT/ TUPLE
+m = grades[1]             #Here we have assigned the value of second lowest score to the variable m
 
 name = []
 
 for val in res:
     if m == val[1]:
-        name.append(val[0])
+        name.append(val[0])    #Here we are acessing the list with val and using the index we are accessing the name and appending it into the name list
         
-name.sort()
+name.sort()    #Sort is only for the list Type
 
 for i in name:
     print(i)
