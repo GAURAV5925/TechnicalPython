3aeec9c557b96efe0cbf6ce06562ac10cc293db2
Gaurav Jha <80048862+GAURAV5925@users.noreply.github.com>
2023-02-26 00:02:13 +0530

    Update RunnerUp.py

--- HackerRank/Day1/RunnerUp.py
+++ HackerRank/Day1/RunnerUp.py
@@ -1,6 +1,10 @@
+'''
+Here the concept of the List is Learned
+'''
+
 if __name__ == '__main__':
     n = int(input())
-    arr = map(int, input().split())
+    arr = map(int, input().split()) # .split() function will split the string into a list type
     
 
-    print(sorted(set(arr))[-2])
+    print(sorted(set(arr))[-2])  #Here we first converting the list into a set then we are taking the second highest value using the negative indexing
