1d83bc62f8b80fff3fb17d487f448c6c8a0e7e2a
Gaurav Jha <80048862+GAURAV5925@users.noreply.github.com>
2023-02-28 22:38:02 +0530

    Update DesignerDoorMat.py

--- HackerRank/Day4/DesignerDoorMat.py
+++ HackerRank/Day4/DesignerDoorMat.py
@@ -4,10 +4,10 @@ s1 = ".|."
 s2 = "WELCOME"
 
 for i in range(n//2):
-    print((s1*((i*2)+1)).center(m,'-'))
+    print((s1 * (( i * 2) + 1)).center(m,'-'))
     
 print(s2.center(m,'-'))
 
-for i in range(n//2-1,-1,-1):
+for i in range(n//2-1,-1,-1):   #Here we have to iterate reverse order hence [Start = ending element,end = ending - 1 as the last element is exclusive, -1 for reversing]
     print((s1*((i*2)+1)).center(m,'-'))
     
