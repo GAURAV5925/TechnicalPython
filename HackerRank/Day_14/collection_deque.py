72d1c5ada476bf0c32dab2b52677b89bd085a994
Gaurav Jha <80048862+GAURAV5925@users.noreply.github.com>
2023-03-09 22:40:17 +0530

    Create collection_deque.py

---
+++ HackerRank/Day_14/collection_deque.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+n = int(input())
+d  = deque()
+
+for i in range(n):
+    c = input().split()
+    
+    if c[0]=='append':
+        d.append(int(c[1]))
+    elif c[0] == 'appendleft':
+        d.appendleft(int(c[1]))
+    elif c[0] == 'pop':
+        d.pop()
+    else:
+        d.popleft()
+        
+print(*d) #Unpack Operator
